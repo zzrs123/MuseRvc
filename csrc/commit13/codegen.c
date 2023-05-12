f40708f440b9d55681d157c1cb16ad5bbff803e7
zzrs123 <2296704779@qq.com>
2023-05-12 18:32:14 +0800

    step12: support "{}"(compound_stmt)
    - corresponding commit13
    - record
    - data structure(in my muservc) is too complex(seems ugly).

---
+++ csrc/commit13/codegen.c
@@ -0,0 +1,210 @@
+#include "rvcc.h"
+
+// 记录栈深度
+static int Depth;
+
+// 压栈，将结果临时压入栈中备用
+// sp为栈指针，栈反向向下增长，64位下，8个字节为一个单位，所以sp-8
+// 当前栈指针的地址就是sp，将a0的值压入栈
+// 不使用寄存器存储的原因是因为需要存储的值的数量是变化的。
+static void push(void) {
+  printf("  addi sp, sp, -8\n");
+  printf("  sd a0, 0(sp)\n");
+  Depth++;
+}
+
+// 弹栈，将sp指向的地址的值，弹出到a1
+static void pop(char *Reg) {
+  printf("  ld %s, 0(sp)\n", Reg);
+  printf("  addi sp, sp, 8\n");
+  Depth--;
+}
+
+// 对齐到Align的整数倍
+static int alignTo(int N, int Align) {
+  // (0,Align]返回Align
+  return (N + Align - 1) / Align * Align;
+}
+
+// 计算给定节点的绝对地址
+// 如果报错，说明节点不在内存中
+static void genAddr(Node *Nd) {
+  if (Nd->Kind == ND_VAR) {
+    // 偏移量是相对于fp的
+    printf("  addi a0, fp, %d\n", Nd->Var->Offset);
+    return;
+  }
+
+  error("not an lvalue");
+}
+
+// 生成表达式
+static void genExpr(Node *Nd) {
+  // 生成各个根节点
+  switch (Nd->Kind) {
+  // 加载数字到a0
+  case ND_NUM:
+    printf("  li a0, %d\n", Nd->Val);
+    return;
+  // 对寄存器取反
+  case ND_NEG:
+    genExpr(Nd->LHS);
+    // neg a0, a0是sub a0, x0, a0的别名, 即a0=0-a0
+    printf("  neg a0, a0\n");
+    return;
+  // 变量
+  case ND_VAR:
+    // 计算出变量的地址，然后存入a0
+    genAddr(Nd);
+    // 访问a0地址中存储的数据，存入到a0当中
+    printf("  ld a0, 0(a0)\n");
+    return;
+  // 赋值
+  case ND_ASSIGN:
+    // 左部是左值，保存值到的地址
+    genAddr(Nd->LHS);
+    push();
+    // 右部是右值，为表达式的值
+    genExpr(Nd->RHS);
+    pop("a1");
+    printf("  sd a0, 0(a1)\n");
+    return;
+  default:
+    break;
+  }
+
+  // 递归到最右节点
+  genExpr(Nd->RHS);
+  // 将结果压入栈
+  push();
+  // 递归到左节点
+  genExpr(Nd->LHS);
+  // 将结果弹栈到a1
+  pop("a1");
+
+  // 生成各个二叉树节点
+  switch (Nd->Kind) {
+  case ND_ADD: // + a0=a0+a1
+    printf("  add a0, a0, a1\n");
+    return;
+  case ND_SUB: // - a0=a0-a1
+    printf("  sub a0, a0, a1\n");
+    return;
+  case ND_MUL: // * a0=a0*a1
+    printf("  mul a0, a0, a1\n");
+    return;
+  case ND_DIV: // / a0=a0/a1
+    printf("  div a0, a0, a1\n");
+    return;
+  case ND_EQ:
+  case ND_NE:
+    // a0=a0^a1，异或指令
+    printf("  xor a0, a0, a1\n");
+
+    if (Nd->Kind == ND_EQ)
+      // a0==a1
+      // a0=a0^a1, sltiu a0, a0, 1
+      // 等于0则置1
+      printf("  seqz a0, a0\n");
+    else
+      // a0!=a1
+      // a0=a0^a1, sltu a0, x0, a0
+      // 不等于0则置1
+      printf("  snez a0, a0\n");
+    return;
+  case ND_LT:
+    printf("  slt a0, a0, a1\n");
+    return;
+  case ND_LE:
+    // a0<=a1等价于
+    // a0=a1<a0, a0=a0^1
+    printf("  slt a0, a1, a0\n");
+    printf("  xori a0, a0, 1\n");
+    return;
+  default:
+    break;
+  }
+
+  error("invalid expression");
+}
+
+// 生成语句
+static void genStmt(Node *Nd) {
+  switch (Nd->Kind) {
+  // 生成代码块，遍历代码块的语句链表
+  case ND_BLOCK:
+    for (Node *N = Nd->Body; N; N = N->Next)
+      genStmt(N);
+    return;
+  // 生成return语句
+  case ND_RETURN:
+    genExpr(Nd->LHS);
+    // 无条件跳转语句，跳转到.L.return段
+    // j offset是 jal x0, offset的别名指令
+    printf("  j .L.return\n");
+    return;
+  // 生成表达式语句
+  case ND_EXPR_STMT:
+    genExpr(Nd->LHS);
+    return;
+  default:
+    break;
+  }
+
+  error("invalid statement");
+}
+
+// 根据变量的链表计算出偏移量
+static void assignLVarOffsets(Function *Prog) {
+  int Offset = 0;
+  // 读取所有变量
+  for (Obj *Var = Prog->Locals; Var; Var = Var->Next) {
+    // 每个变量分配8字节
+    Offset += 8;
+    // 为每个变量赋一个偏移量，或者说是栈中地址
+    Var->Offset = -Offset;
+  }
+  // 将栈对齐到16字节
+  Prog->StackSize = alignTo(Offset, 16);
+}
+
+// 代码生成入口函数，包含代码块的基础信息
+void codegen(Function *Prog) {
+  assignLVarOffsets(Prog);
+  printf("  .globl main\n");
+  printf("main:\n");
+
+  // 栈布局
+  //-------------------------------// sp
+  //              fp
+  //-------------------------------// fp = sp-8
+  //             变量
+  //-------------------------------// sp = sp-8-StackSize
+  //           表达式计算
+  //-------------------------------//
+
+  // Prologue, 前言
+  // 将fp压入栈中，保存fp的值
+  printf("  addi sp, sp, -8\n");
+  printf("  sd fp, 0(sp)\n");
+  // 将sp写入fp
+  printf("  mv fp, sp\n");
+
+  // 偏移量为实际变量所用的栈大小
+  printf("  addi sp, sp, -%d\n", Prog->StackSize);
+
+  // 生成语句链表的代码
+  genStmt(Prog->Body);
+  assert(Depth == 0);
+
+  // Epilogue，后语
+  // 输出return段标签
+  printf(".L.return:\n");
+  // 将fp的值改写回sp
+  printf("  mv sp, fp\n");
+  // 将最早fp保存的值弹栈，恢复fp。
+  printf("  ld fp, 0(sp)\n");
+  printf("  addi sp, sp, 8\n");
+  // 返回
+  printf("  ret\n");
+}
---
+++ csrc/commit13/rust/lexer.rs
@@ -0,0 +1,141 @@
+/*=====================================================================
+                        词法分析
+// ====================================================================== */
+
+use crate::{error, parser};
+use crate::error::verror_at;
+// use crate::parser::equal;
+/*=====================================================================
+                        Token系统
+// ====================================================================== */
+#[derive(PartialEq, Clone)]
+pub enum TokenKind {
+    TkIdent, // 标记符，可以为变量名、函数名等
+    TkPunct, // 操作符如： + -
+    TkKeyword, // 关键字
+    TkNum,   // 数字
+    TKEof,   // 文件终止符，即文件的最后
+}
+
+pub enum V<'a> {
+    Int(i32),
+    Str(&'a str), //step9：2023/05/05 感觉这里用&str不优雅，后续看看能不能用enum嵌套或者String
+}
+#[allow(dead_code)]
+// 终结符结构体
+pub struct Token<'a> {
+    pub kind: TokenKind,
+    pub value: Option<V<'a>>,
+    pub loc: usize,
+    pub len: usize,
+}
+/*======================================================================
+                    convert_keywords: 关键词识别函数
+                将tokens序列中的关键词识别出来转化为keword类型
+====================================================================== */
+fn convert_keywords(tokens:&mut Vec<Token>){
+    for tok in tokens.iter_mut() {
+        if parser::equal(&tok, "return") {
+            tok.kind = TokenKind::TkKeyword;
+        }
+    }
+}
+
+/*======================================================================
+                    toknize: Token解析主干函数
+            从头到尾扫描args[1]，对不同类型的token做不同处理
+                rust用match+vec<T>实现起来是相当优雅的
+
+                step9：2023/05/05：太长了这个函数
+====================================================================== */
+pub fn tokenize(arg: &mut str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut start = 0;
+    // let mut iter = arg.char_indices().peekable();
+    // arg.char_indices()同时得到索引和字符，对得到的字符用match进行处理
+    for (i, c) in arg.char_indices() {
+        if i < start {
+            continue; //跳过已经匹配的字符
+        }
+        match c {
+            // 处理空白字符
+            c if c.is_whitespace() => {
+                start = i + 1;
+                continue;
+            },
+            // 解析操作符
+            // 特点是长度一定为1或2
+            c if c.is_ascii_punctuation() => {
+                let mut punct_len = 1;
+                if let Some(next_char) = arg.chars().nth(i + 1) {
+                    if (c == '!' && next_char == '=') || (c == '=' && next_char == '=') || (c == '<' && next_char == '=') || (c == '>' && next_char == '=') {
+                        punct_len = 2;
+                    }
+                }
+                let str1 = &arg[start..=i+punct_len-1];
+                let token = Token {
+                    kind: TokenKind::TkPunct,
+                    value: Some(V::Str(str1)),
+                    loc: i,
+                    len: punct_len,
+                };
+                tokens.push(token);
+                start = i + punct_len;
+            },
+            // 解析数字
+            '0'..='9' =>  {
+                let mut end = i;
+                while let Some(c) = arg.chars().nth(end) {
+                    if c.is_digit(10) {
+                        end += 1;
+                    } else {
+                        break;
+                    }
+                }
+                let numeric = arg[start..end].parse::<i32>().ok();
+                let token = Token {
+                    kind: TokenKind::TkNum,
+                    value: Some(V::Int(numeric.unwrap())),
+                    loc: i,
+                    len: end - start,
+                };
+                tokens.push(token);
+                start = end;
+            }
+            // 解析字母（变量），同时预留了多字母变量的支持
+            'a'..='z' | 'A'..='Z' | '_'  => {
+                let mut end = i;
+                while let Some(c) = arg.chars().nth(end) {
+                    if c.is_ascii_alphabetic() || c.is_ascii_digit() || c == '_' {
+                        end += 1;
+                    } else {
+                        break;
+                    }
+                }
+                let ident = &arg[start..end];
+                let token = Token {
+                    kind: TokenKind::TkIdent,
+                    value: Some(V::Str(ident)),
+                    loc: i,
+                    len: end - start,
+                };
+                tokens.push(token);
+                start = end;
+            }
+            _ => {
+                // 处理无法识别的字符
+                let loc_char: usize = i;
+                error!(loc_char,"Unexpected character '{}'", c);
+            }
+        }
+    }
+    let eof_token = Token {
+        kind: TokenKind::TKEof,
+        value: None,
+        loc: arg.len(),
+        len: 0,
+    };
+    tokens.push(eof_token);
+    convert_keywords(&mut tokens);
+    tokens
+}
