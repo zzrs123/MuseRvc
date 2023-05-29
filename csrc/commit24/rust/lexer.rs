c757d57c35966d44b58d13c023c91a9ec2dbe47d
zzrs123 <2296704779@qq.com>
2023-05-29 18:28:10 +0800

    step23: support max-6-args functions-call
    - correspongding commit24
    - record
    - 我有些许深情的独白,有意避开了人海

---
+++ csrc/commit24/rust/lexer.rs
@@ -0,0 +1,163 @@
+/*=====================================================================
+                        词法分析
+// ====================================================================== */
+
+// use crate::parser::equal;
+use crate::{error};
+use crate::error::verror_at;
+// use crate::parser::equal;
+/*=====================================================================
+                        Token系统
+// ====================================================================== */
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenKind {
+    TkIdent, // 标记符，可以为变量名、函数名等
+    TkPunct, // 操作符如： + -
+    TkKeyword, // 关键字
+    TkNum,   // 数字
+    TKEof,   // 文件终止符，即文件的最后
+}
+#[derive(Debug, PartialEq, Clone)]
+pub enum V{
+    Int(i32),
+    Str(String), //step9：2023/05/05 感觉这里用&str不优雅，后续看看能不能用enum嵌套或者String
+    // step21: 丑陋的就是不正确的，&str类型会带来很多生命周期问题，果断换掉
+}
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+// 终结符结构体
+pub struct Token {
+    pub kind: TokenKind,
+    pub value: Option<V>,
+    pub loc: usize,
+    pub len: usize,
+}
+/*======================================================================
+                    is_keywords: 关键词判断函数
+                维护一个关键词表，传入token返回true false
+====================================================================== */
+fn is_keywords(token: &Token) -> bool{
+    let keys = vec!["return", "if", "else", "for", "while", "int"];
+    if let Some(V::Str(st)) = &token.value{
+        if keys.contains(&st.as_str()){
+            return true;
+        }else{
+            return false
+        }
+    }
+    false
+}
+
+/*======================================================================
+                    convert_keywords: 关键词识别函数
+                将tokens序列中的关键词识别出来转化为keword类型
+====================================================================== */
+fn convert_keywords(tokens:&mut Vec<Token>){
+    for tok in tokens.iter_mut() {
+        // if parser::equal(&tok, "return") {
+        //     tok.kind = TokenKind::TkKeyword;
+        // }
+        if is_keywords(&tok){
+            tok.kind = TokenKind::TkKeyword;
+        }
+    }
+}
+
+/*======================================================================
+                    toknize: Token解析主干函数
+            从头到尾扫描args[1]，对不同类型的token做不同处理
+                rust用match+vec<T>实现起来是相当优雅的
+
+                step9：2023/05/05：太长了这个函数
+====================================================================== */
+pub fn tokenize(arg: &mut str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut start = 0;
+    // let mut iter = arg.char_indices().peekable();
+    // arg.char_indices()同时得到索引和字符，对得到的字符用match进行处理
+    for (i, c) in arg.char_indices() {
+        if i < start {
+            continue; //跳过已经匹配的字符
+        }
+        match c {
+            // 处理空白字符
+            c if c.is_whitespace() => {
+                start = i + 1;
+                continue;
+            },
+            // 解析操作符
+            // 特点是长度一定为1或2
+            c if c.is_ascii_punctuation() => {
+                let mut punct_len = 1;
+                if let Some(next_char) = arg.chars().nth(i + 1) {
+                    if (c == '!' && next_char == '=') || (c == '=' && next_char == '=') || (c == '<' && next_char == '=') || (c == '>' && next_char == '=') {
+                        punct_len = 2;
+                    }
+                }
+                let str1 = &arg[start..=i+punct_len-1];
+                let token = Token {
+                    kind: TokenKind::TkPunct,
+                    value: Some(V::Str(str1.to_owned())),
+                    loc: i,
+                    len: punct_len,
+                };
+                tokens.push(token);
+                start = i + punct_len;
+            },
+            // 解析数字
+            '0'..='9' =>  {
+                let mut end = i;
+                while let Some(c) = arg.chars().nth(end) {
+                    if c.is_digit(10) {
+                        end += 1;
+                    } else {
+                        break;
+                    }
+                }
+                let numeric = arg[start..end].parse::<i32>().ok();
+                let token = Token {
+                    kind: TokenKind::TkNum,
+                    value: Some(V::Int(numeric.unwrap())),
+                    loc: i,
+                    len: end - start,
+                };
+                tokens.push(token);
+                start = end;
+            }
+            // 解析字母（变量），同时预留了多字母变量的支持
+            'a'..='z' | 'A'..='Z' | '_'  => {
+                let mut end = i;
+                while let Some(c) = arg.chars().nth(end) {
+                    if c.is_ascii_alphabetic() || c.is_ascii_digit() || c == '_' {
+                        end += 1;
+                    } else {
+                        break;
+                    }
+                }
+                let ident = &arg[start..end];
+                let token = Token {
+                    kind: TokenKind::TkIdent,
+                    value: Some(V::Str(ident.to_owned())),
+                    loc: i,
+                    len: end - start,
+                };
+                tokens.push(token);
+                start = end;
+            }
+            _ => {
+                // 处理无法识别的字符
+                let loc_char: usize = i;
+                error!(loc_char,"Unexpected character '{}'", c);
+            }
+        }
+    }
+    let eof_token = Token {
+        kind: TokenKind::TKEof,
+        value: None,
+        loc: arg.len(),
+        len: 0,
+    };
+    tokens.push(eof_token);
+    convert_keywords(&mut tokens);
+    tokens
+}
---
+++ csrc/commit24/rust/typecheck.rs
@@ -0,0 +1,150 @@
+use crate::{ast::*, lexer::Token};
+use crate::error::verror_at;   
+use crate::error;
+/*======================================================================
+                            Type 类型推理系统
+                    后续可能会成为一个类型推理、检测系统
+ =======================================================================*/
+ 
+// 类型种类
+#[derive(Debug, PartialEq, Clone)]
+pub enum TypeKind {
+    // Unchecked, //未定义类型，step20预留，我觉得一定会有这个类型
+    Int, // 整型
+    Ptr, // 指针
+}
+#[derive(Debug, PartialEq, Clone)]
+pub struct Type {
+    pub kind: TypeKind, // 种类
+    pub base: Option<Box<Type>>, // 指向的类型
+    // step21增加，这里困扰了很久
+    pub name: Option<Token>,
+}
+// step20预留，step21启用
+// Pointer type that points to the base class.
+pub fn pointer_to(base: Type) -> Type {
+    Type { 
+        base: Some(Box::new(base)),
+        kind: TypeKind::Ptr,
+        name: None, 
+    }
+}
+// 为节点内的所有节点添加类型
+
+impl Type{
+    pub fn new(kind: TypeKind) -> Self {
+        Self { kind, base: None, name: None }
+    }
+    // 半废弃，不如is_pointer来的方便，并且这个函数在new_sub中无法通过形如*(&y-1)的运算，
+    // 也就是左侧是指针类型，右侧是Int类型，但是base_ty会把左侧判否
+    // ,后续考虑删掉这个函数
+    pub fn base_ty(&self) -> Option<&Self> {
+        match &self.base {
+            Some(ty) => ty.base_ty(),
+            None => None,
+        }
+    }
+    // ==== 匆忙放进impl的野函数，没有用self =============
+    // ===============================================
+    // 判断是否为整型
+    pub fn is_integer(ty: &Type) -> bool {
+        matches!(ty.kind, TypeKind::Int)
+    }
+    // 判断是否为指针
+    pub fn is_pointer(ty: &Type) -> bool{
+        matches!(ty.kind, TypeKind::Ptr)
+    }
+    /*======================================================================
+                                add_type 类型推理核心函数
+    =======================================================================*/
+    pub fn add_type(mut n: &mut Box<Node>)-> Box<Node> {
+        if n.ty.is_none() {
+            // 递归访问所有节点以增加类型
+            if let Some(lhs) = n.lhs.clone().as_mut() {
+                n.lhs = Some(Type::add_type(lhs));
+            }
+            if let Some(rhs) = n.rhs.clone().as_mut() {
+                n.rhs = Some(Type::add_type(rhs));
+            }
+            if let Some(cond) = n.cond.clone().as_mut() {
+                n.cond = Some(Type::add_type(cond));
+            }
+            if let Some(then) = n.then.clone().as_mut() {
+                n.then = Some(Type::add_type(then));
+            }
+            if let Some(els) = n.els.clone().as_mut() {
+                n.els = Some(Type::add_type(els));
+            }
+            if let Some(init) = n.init.clone().as_mut() {
+                n.init = Some(Type::add_type(init));
+            }
+            if let Some(inc) = n.inc.clone().as_mut() {
+                n.inc = Some(Type::add_type(inc));
+            }
+            // 访问链表内的所有节点以增加类型
+            let mut body = n.body.clone();
+            while let Some(mut node) = body {
+                let mut node = Type::add_type(&mut node);
+                body = node.next.take();
+            }
+            // 设置节点类型
+            match n.kind {
+                // 将节点类型设为 节点左部的类型
+                NodeKind::NdAdd | NodeKind::NdSub | NodeKind::NdMul | NodeKind::NdDiv | NodeKind::NdNeg | 
+                NodeKind::NdAssign => {
+                    n.ty = n.lhs.as_ref().and_then(|lhs| lhs.ty.clone());
+                    //let node = n;
+                    n.clone()
+                }
+                // 将节点类型设为 int
+                NodeKind::NdEq | NodeKind::NdNeq | NodeKind::NdLt | NodeKind::NdLe |NodeKind::NdNum |
+                NodeKind::NdFuncall => {
+                    n.ty = Some(Type {
+                        kind: TypeKind::Int,
+                        base: None,
+                        name: None,
+                    });
+                    n.clone()
+                }
+                NodeKind::NdVar => {
+                    // n.ty = n.clone().var.unwrap().ty;
+                    n.ty = n.var.as_ref().and_then(|var| var.ty.clone());
+                    n.clone()
+                }
+                // 将节点类型设为 指针，并指向左部的类型
+                NodeKind::NdAddr => {
+                    n.ty = n.lhs.as_ref().and_then(|lhs| {
+                        Some(Type {
+                            kind: TypeKind::Ptr,
+                            base: Some(Box::new(lhs.ty.as_ref().unwrap().clone())),
+                            name: None,
+                        })
+                    });
+                    n.clone()
+                }
+                // 节点类型：如果解引用指向的是指针，则为指针指向的类型；否则为int
+                NodeKind::NdDeref => {
+                    if let Some(lhs_ty) = n.lhs.as_ref().and_then(|lhs| lhs.ty.as_ref()) {
+                        n.ty = Some(if lhs_ty.kind == TypeKind::Ptr {
+                            *lhs_ty.base.as_ref().unwrap().clone()
+                        } else {
+                            // Type {
+                            //     kind: TypeKind::Int,
+                            //     base: None,
+                            //     name: None,
+                            // }
+                            let loc = n.tokloc;
+                            error!( loc,"invalid pointer dereference {}", "here");
+                        });
+                    }
+                    n.clone()
+                }
+                // 其他情况不设置类型
+                _ => {n.clone()}
+            }
+        }else{
+            n.clone()
+        }
+    }
+}
+
